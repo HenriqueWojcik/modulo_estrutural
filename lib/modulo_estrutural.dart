e210885069b502c3e08f3e8fead4766a5b173679
Henrique Wojcik <henriquewojcik@brq.com>
2022-10-13 16:00:07 -0300

    ok

--- lib/modulo_estrutural.dart
+++ lib/modulo_estrutural.dart
@@ -3,7 +3,7 @@ library modulo_estrutural;
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
-  int addOne(num value) => value.toInt() + 1;
+  int addOne(String value) => (value as int) + 1;
 
   int removeOne(int value) => value - 1;
 }
